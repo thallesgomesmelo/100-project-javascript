1ba800e157eb27ba90650709bacc6e6377ac5d41
Thalles <thalles.melo@mestria.com.br>
2023-08-24 10:14:08 -0300

    Feito layout e lógica do jogo.

---
+++ 27 - Mine Sweeper Game/js/index.js
@@ -0,0 +1,122 @@
+const grid = document.getElementById("grid");
+let lockGame = false;
+const testMode = false;
+generateGrid();
+
+// Generate 10 * 10 grid
+function generateGrid() {
+  lockGame = false;
+  grid.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    row = grid.insertRow(i);
+
+    for (let j = 0; j < 10; j++) {
+      cell = row.insertCell(j);
+      cell.onclick = function () {
+        init(this);
+      };
+
+      let mine = document.createAttribute("mine");
+      mine.value = "false";
+      cell.setAttributeNode(mine);
+    }
+  }
+
+  generateMines();
+}
+
+// Generate mines randomly
+function generateMines() {
+  // Add 20 mines to game
+  for (let i = 0; i < 20; i++) {
+    let row = Math.floor(Math.random() * 10);
+    let col = Math.floor(Math.random() * 10);
+    let cell = grid.rows[row].cells[col];
+    cell.setAttribute("mine", "true");
+
+    if (testMode) {
+      cell.innerHTML = "X";
+    }
+  }
+}
+
+// Highlight all mines red
+function revealMines() {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      let cell = grid.rows[i].cells[j];
+
+      if (cell.getAttribute("mine") == "true") {
+        cell.className = "mine";
+      }
+    }
+  }
+}
+
+function checkGameComplete() {
+  let gameComplete = true;
+
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      if (
+        grid.rows[i].cells[j].getAttribute("mine") == "false" &&
+        grid.rows[i].cells[j].innerHTML == ""
+      ) {
+        gameComplete = false;
+      }
+    }
+  }
+
+  if (gameComplete) {
+    alert("You Found All Mines!");
+    revealMines();
+  }
+}
+
+function init(cell) {
+  // Check game completed or no
+  if (lockGame) {
+    return;
+  } else {
+    // Check user clicked on mine
+    if (cell.getAttribute("mine") == "true") {
+      revealMines();
+      lockGame = "true";
+    } else {
+      cell.className = "active";
+
+      // Display number of mines around cell
+      let mineCount = 0;
+      let cellRow = cell.parentNode.rowIndex;
+      let cellCol = cell.cellIndex;
+
+      for (let i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
+        for (let j = Math.max(cellCol - 1, 0); j <= Math.min(cellCol + 1, 9); j++) {
+          if (grid.rows[i].cells[j].getAttribute("mine") == "true") {
+            mineCount++;
+          }
+        }
+      }
+
+      cell.innerHTML = mineCount;
+
+      // If cell don't have mine
+      if (mineCount == 0) {
+        for (let i = Math.max(cellRow - 1, 0); i <= Math.min(cellRow + 1, 9); i++) {
+          for (
+            let j = Math.max(cellCol - 1, 0);
+            j <= Math.min(cellCol + 1, 9);
+            j++
+          ) {
+            if (grid.rows[i].cells[j].innerHTML == "") {
+              init(grid.rows[i].cells[j]);
+            }
+          }
+        }
+      }
+
+      checkGameComplete();
+    }
+  }
+}
